86e25def693cac3d2f1d28a2ba0b3b87c17624f2
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-03-20 16:26:10 -0300

    Update Triangle.java

--- 2) Classes/3-Packages/3.1/figures/Triangle.java
+++ 2) Classes/3-Packages/3.1/figures/Triangle.java
@@ -9,12 +9,12 @@ public class Triangle {
 
     public Triangle (int x1,int x2,int x3,int y1,int y2,int y3, Color fundo) {
         this.x1 = x1;
-		this.x2 = x2;
-		this.x3 = x3;
+	this.x2 = x2;
+	this.x3 = x3;
         this.y1 = y1;
-		this.y2 = y2;
-		this.y3 = y3;
-		this.fundo = fundo;
+	this.y2 = y2;
+	this.y3 = y3;
+	this.fundo = fundo;
     }
 
     public void paint (Graphics g) {
