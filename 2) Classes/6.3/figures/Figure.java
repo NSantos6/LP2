277d9a9a0be34853ba9cb4cfa1cafbaa8343d55f
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-04-08 20:42:10 -0300

    Update Figure.java

--- 2) Classes/6.3/figures/Figure.java
+++ 2) Classes/6.3/figures/Figure.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public abstract class Figure {
-	int x, y, w, h, x1, x2, x3, y1, y2, y3;
-	Color contorno, fundo;
 	public abstract void print ();
     public abstract void paint (Graphics g);
 }
