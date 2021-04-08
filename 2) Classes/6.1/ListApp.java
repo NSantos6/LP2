c2bf142a8f13430665e58447c052518bb6e27390
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-04-07 21:04:53 -0300

    Update ListApp.java

--- 2) Classes/6.1/ListApp.java
+++ 2) Classes/6.1/ListApp.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 import figures.*;
 
-class ListApp {
+class ListApp2 {
     public static void main (String[] args) {
         ListFrame frame = new ListFrame();
         frame.setVisible(true);
@@ -14,7 +14,7 @@ class ListApp {
 }
 
 class ListFrame extends JFrame {
-    ArrayList<Figure> figs = new ArrayList<Figure>(); //Lista unica de figuras
+    ArrayList<Figure> figs = new ArrayList<Figure>();
     Random rand = new Random();
 
     ListFrame () {
@@ -29,32 +29,34 @@ class ListFrame extends JFrame {
         this.addKeyListener (
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
-                    int x = rand.nextInt(300);
-                    int y = rand.nextInt(300);
+                    int x = rand.nextInt(350);
+                    int y = rand.nextInt(350);
                     int w = rand.nextInt(70);
                     int h = rand.nextInt(70);
-                    int x1 = rand.nextInt(65);
-                    int x2 = rand.nextInt(65);
-                    int x3 = rand.nextInt(65);
-                    int y1 = rand.nextInt(65);
-                    int y2 = rand.nextInt(65);
-                    int y3 = rand.nextInt(65);
-                    Color contorno = new Color(rand.nextInt(200), rand.nextInt(200), rand.nextInt(200));
+                    
+                    int x1 = rand.nextInt(350);
+                    int x2 = rand.nextInt(350);
+                    int x3 = rand.nextInt(350);
+                    int y1 = rand.nextInt(350);
+                    int y2 = rand.nextInt(350);
+                    int y3 = rand.nextInt(350);
+                    
+                    Color contorno = new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
                     Color fundo = new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
                     if (evt.getKeyChar() == 'r') {
-                        figs.add(new Rect(x, y, w, h, contorno, fundo)); //Retangulo
+                        figs.add(new Rect(x, y, w, h));
                     } else if (evt.getKeyChar() == 'e') {
-                        figs.add(new Ellipse(x, y, w, h, contorno, fundo)); //Ellipse
+                        figs.add(new Ellipse(x, y, w, h));
                     } else if (evt.getKeyChar() == 't') {
-                    	figs.add(new Triangle(x1, x2, x3, y1, y2, y3, fundo)); //Triangulo
+                    	figs.add(new Triangl(x1, x2, x3, y1, y2,y3, fundo));
                     }
                     repaint();
                 }
             }
         );
 
-        this.setTitle("Lista de Figuras Misturadas");
-        this.setSize(400, 400);
+        this.setTitle("Lista de Figuras Heterogenea");
+        this.setSize(410, 410);
     }
 
     public void paint (Graphics g) {
