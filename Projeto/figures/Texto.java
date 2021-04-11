67a3343157f94c5c569a90f2560c765bc9aa2a03
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-04-11 19:57:41 -0300

    Create Texto.java

---
+++ Projeto/figures/Texto.java
@@ -0,0 +1,19 @@
+package figures;
+
+import java.awt.*;
+public  class Texto extends Figure{
+    private String palavra, fonte;
+	private int tamanho;
+    public Texto (String palavra, int x, int y, int tamanho, Color fundo) {
+		this.palavra = palavra;
+        this.fonte = fonte;
+		this.tamanho = tamanho;
+    }
+    @Override
+    public void paint (Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+        g2d.setFont(new Font(fonte, Font.ITALIC, tamanho));
+        g2d.setColor(fundo);
+		g2d.drawString(palavra,x,y);
+    }
+}
