1920ac0e11c3ed477a2c62c76dcea710c7dad6a1
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-04-11 19:59:23 -0300

    Update Texto.java

--- Projeto/figures/Texto.java
+++ Projeto/figures/Texto.java
@@ -5,15 +5,15 @@ public  class Texto extends Figure{
     private String palavra, fonte;
 	private int tamanho;
     public Texto (String palavra, int x, int y, int tamanho, Color fundo) {
-		this.palavra = palavra;
+	this.palavra = palavra;
         this.fonte = fonte;
-		this.tamanho = tamanho;
+	this.tamanho = tamanho;
     }
     @Override
     public void paint (Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
+	Graphics2D g2d = (Graphics2D) g;
         g2d.setFont(new Font(fonte, Font.ITALIC, tamanho));
         g2d.setColor(fundo);
-		g2d.drawString(palavra,x,y);
+	g2d.drawString(palavra,x,y);
     }
 }
