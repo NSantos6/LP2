4cae786726c23c488f98e6e0cf731b20a09f0a66
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-04-11 21:58:45 -0300

    Update app.java

--- Projeto/app.java
+++ Projeto/app.java
@@ -52,7 +52,6 @@ class ListFrame extends JFrame {
                     } else if (evt.getKeyChar() == 't') {
                     	figs.add(new Triangl(x1, x2, x3, y1, y2,y3, fundo));
                     } else if (evt.getKeyChar() == 'p'){
-
                         figs.add(new Texto("Olá, teste", x, y, tamanho, "Calibri", fundo));
                     }
                     repaint();
