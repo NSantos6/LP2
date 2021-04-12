16c9335f045c711b12b0a4b675d9b0defac861e1
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-04-11 22:15:05 -0300

    Update Texto.java

--- Projeto/figures/Texto.java
+++ Projeto/figures/Texto.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public  class Texto extends Figure{
     	String palavra, fonte;
 	int tamanho;
-    	public Texto (String palavra, int x, int y, int tamanho, String, fonte, Color fundo) {
+    	public Texto (String palavra, int tamanho, String, fonte, Color fundo) {
 		this.palavra = palavra;
         	this.fonte = fonte;
 		this.tamanho = tamanho;
@@ -14,6 +14,6 @@ public  class Texto extends Figure{
 	Graphics2D g2d = (Graphics2D) g;
         g2d.setFont(new Font(fonte, Font.ITALIC, tamanho));
         g2d.setColor(fundo);
-	g2d.drawString(palavra,x,y);
+	g2d.drawString(palavra);
     }
 }
