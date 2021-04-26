1a07503e3d9154a2df1e2af965a1898d0aef9df0
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-04-25 22:33:36 -0300

    Update Ellipse.java

--- Projeto/figures/Ellipse.java
+++ Projeto/figures/Ellipse.java
@@ -7,13 +7,23 @@ import java.awt.geom.Ellipse2D.Double;
 public class Ellipse extends Figure {
     int x, y;
     int w, h;
+    Color contorno;
+    Color fundo;
 
-    public Ellipse (int x, int y, int w, int h) {
+    public Ellipse (int x, int y, int w, int h, Color contorno, Color fundo) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.contorno = contorno;
+        this.fundo = fundo;
     }
+
+    public void drag(int dx, int dy){
+        this.x += dx;
+        this.y += dy;
+    }
+    
     @Override
     public void print () {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
