a93b27fe7491ea0d15eed4952f8eb53c18cba15a
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-04-25 22:32:55 -0300

    Update Figure.java

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -5,8 +5,17 @@ import java.awt.Graphics;
 
 public abstract class Figure {
 	public abstract void print ();
-    int x, y, w, h;
-    Color contorno;
-    Color fundo;
+    public int x, y, w, h;
+    public Color contorno;
+    public Color fundo;
+    public void drag(int dx, int dy){
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public void aumento(int largura, int altura){
+		this.w += largura;
+		this.h += altura;
+	}
     public abstract void paint (Graphics g);
 }
