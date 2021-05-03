5c4d8893645b1f16b8afd1b220103f27084da713
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-05-03 20:37:49 -0300

    Create veiculo.java

---
+++ 2) Classes/6.2/veiculo.java
@@ -0,0 +1,21 @@
+class Vehicle{
+    public void move(){
+    System.out.println(“Vehicles can move!!”);
+    }
+}
+
+class MotorBike extends Vehicle{
+    @Override
+    public void move(){
+    System.out.println(“MotorBike can move and accelerate too!!”);
+    }
+}
+
+class Test{
+    public static void main(String[] args){
+    Vehicle vh=new MotorBike();
+    vh.move();    
+    vh=new Vehicle();
+    vh.move();   
+    }
+}
