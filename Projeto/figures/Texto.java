219622f35ec1c9c8588794d056504327aade6d85
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-05-16 19:30:58 -0300

    Update Texto.java

--- Projeto/figures/Texto.java
+++ Projeto/figures/Texto.java
@@ -4,35 +4,29 @@ import java.awt.*;
 import java.util.Random;
 
 public  class Texto extends Figure{
-    private String palavra, fonte;
-	private int tamanho, x, y;
+    public String palavra, fonte;
+    public int fundoR, fundoG, fundoB;
+	public int tamanho, x, y, w, h;
+    public int contornoR, contornoG, contornoB;
     Random rand = new Random();
 
 	public void print () {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.palavra, this.fonte, this.tamanho);
 	}
-    public Texto (String palavra, int x, int y, int tamanho, String fonte, Color fundo) {
-		this.palavra = palavra;
-        this.fonte = fonte;
-		this.tamanho = tamanho;
-		this.x = x;
-		this.y = y;
+
+
+    public Texto (String palavra, String fonte, int x, int y, int w, int h, Color fundo, Color contorno){
+        super(x, y, w, h, fundo, contorno);
     }
 
-    public void drag(int x, int y){
-        this.x += x;
-        this.y += y;
-    }
     
-    @Override
+    //@Override
     public void paint (Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-        g2d.setFont(new Font(fonte, Font.ITALIC, tamanho));
+        g2d.setFont(new Font(fonte, Font.ITALIC, this.x));
         g2d.setColor(new Color (rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
-		g2d.drawString(palavra,x,y);
+		g2d.drawString("alo, teste",x,y);
         
     }
 }
-
-
