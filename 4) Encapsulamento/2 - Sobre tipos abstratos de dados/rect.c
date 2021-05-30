2729f24ae0fb18a36530523a022bb3b6d7c686ff
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-05-30 00:41:50 -0300

    Create rect.c

---
+++ 4) Encapsulamento/2 - Sobre tipos abstratos de dados/rect.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rect.h"
+
+typedef struct Rect{
+	int x, y, w, h;
+}Rect;
+
+Rect* rect_new(void){
+	Rect* this = malloc(sizeof(Rect));
+	this->x = 0;
+	this->y = 0;
+	this->w = 1;
+	this->h = 2;
+}
+
+void rect_drag (Rect* this, int dx, int dy){
+	this->x += dx;
+	this-> y += dy;
+}
+
+void rect_print(Rect* this){
+	printf("Retangulo de tamanho (%d, %d) na posicao (%d, %d). \n", this->w, this->h, this->x, this->y);
+}
