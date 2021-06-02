e8638998e9af4752e7853c2cb07be76c6698c2c5
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-06-01 21:45:20 -0300

    Create RoundRect.java

---
+++ 4) Encapsulamento/3.1) Métodos de Acesso - Getter e Setter/figures/RoundRect.java
@@ -0,0 +1,38 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.geom.RoundRectangle2D.Double;
+
+public class RoundRect extends Figure {
+    private int circleW, circleH;
+
+    public RoundRect (int x, int y, int w, int h, int circleW, int circleH, int contornoR, int contornoG, int contornoB, int fundoR, int fundoG, int fundoB) {
+        super(x,y, w,h,contornoR,contornoG, contornoB, fundoR, fundoG, fundoB);
+	this.circleW = circleW;
+	this.circleH = circleH;
+    }
+
+
+    public void paint (Graphics g, boolean focused) {
+        Graphics2D g2d = (Graphics2D) g;
+	
+	if (focused) {
+	    g2d.setPaint(Color.RED);
+	    g2d.draw(new RoundRectangle2D.Double(this.x-2,this.y-2, this.w+4,this.h+4, this.circleW,this.circleH));
+	}
+	
+	g2d.setColor(new Color(this.contornoR,this.contornoG,this.contornoB));
+	g2d.fill(new RoundRectangle2D.Double(this.x,this.y, this.w,this.h, this.circleW,this.circleH));
+        g2d.setColor(new Color(this.fundoR,this.fundoG,this.fundoB));
+        g2d.draw(new RoundRectangle2D.Double(this.x,this.y, this.w,this.h, this.circleW,this.circleH));
+    }
+
+    public int getcircleW(){
+        return circleW;
+    }
+
+    public int getcircleH(){
+        return circleH;
+    }
+}
